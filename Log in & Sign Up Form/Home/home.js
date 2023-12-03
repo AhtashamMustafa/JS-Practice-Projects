64e51d7d20e4feb5f4d069b4f20ac10575896ca3
Ahtasham Mustafa <ahtshammustufa786@gmail.com>
2023-12-03 18:15:38 +0500

    changes in home page

--- Log in & Sign Up Form/Home/home.js
+++ Log in & Sign Up Form/Home/home.js
@@ -4,23 +4,16 @@ const thirdContainer = document.querySelector("#container3");
 const fourthContainer = document.querySelector("#container4");
 const loggedin = document.querySelector("#loggedinuser");
 const loggedout = document.querySelector("#logout");
-let drop = document.querySelector(".dropdown");
-let username = document.querySelector("#name");
+const notification = document.querySelector("#notification");
+let dropupdate = document.querySelector(".dropdown");
+let drop = document.querySelector(".main-container");
+let username = document.querySelector(".name");
+let postname = document.querySelector("#name");
 let useremail = document.querySelector("#email");
 let usercontact = document.querySelector("#contact");
 let usercity = document.querySelector("#city");
 let updateimg = document.querySelector("#imagePreview");
 let userimg = document.querySelector(".imagePreview");
-
-
-function dropdown() {
-  drop.style.height = "67vh";
-  loggedin.setAttribute("onclick", "dropup()");
-}
-function dropup() {
-  drop.style.height = 0;
-  loggedin.setAttribute("onclick", "dropdown()");
-}
 function classonHandler() {
   // secondContainer.classList.add('visible')
   // secondContainer.classList.remove('hidden')
@@ -56,6 +49,9 @@ if (!loggedInUser) window.location.href = "../Login/Login.html";
 // Display the username on the page
 loggedin.innerHTML = JSON.parse(
   localStorage.getItem("LoggedInuser")
+  )[0].username;
+postname.innerHTML = JSON.parse(
+  localStorage.getItem("LoggedInuser")
 )[0].username;
 
 function logout() {
@@ -67,33 +63,25 @@ username.value = JSON.parse(localStorage.getItem("LoggedInuser"))[0].username;
 useremail.value = JSON.parse(localStorage.getItem("LoggedInuser"))[0].email;
 usercontact.value = JSON.parse(localStorage.getItem("LoggedInuser"))[0].contactNo;
 usercity.value = JSON.parse(localStorage.getItem("LoggedInuser"))[0].city;
-userimg.src = JSON.parse(localStorage.getItem("LoggedInuser"))[0].imzageURL;
-updateimg.src = JSON.parse(localStorage.getItem("LoggedInuser"))[0].imgageURL;
 function update() {
     var users = JSON.parse(localStorage.getItem("users")) || [];
     var loggedInUseremail = JSON.parse( 
       localStorage.getItem("LoggedInuser")
-    ).email; 
-    var image = document.getElementById('profile');
-
-
-  var imgurl=URL.createObjectURL(image.files[0])
- document.querySelector('.imagePreview').src = imgurl
- document.querySelector('#imagePreview').src = imgurl
-
-    var index = -1
-    for (let i = 0; i<users.length;i++){
-      if (users[i].email == useremail.value) {
-        index = i;
-        break
-      }
+      ).email;
+document.querySelector('#imagePreview').addEventListener("click",function(){
+  document.getElementById('profile').click()})
+  var index = -1
+  for (let i = 0; i<users.length;i++){
+    if (users[i].email == useremail.value) {
+      index = i;
+      break
     }
+  }
     if (index !== -1) {
       users[index].username = username.value;
       users[index].email = useremail.value;
       users[index].contactNo = usercontact.value;
       users[index].city = usercity.value;
-      users[index].imgageURL = imgurl
       localStorage.setItem('users', JSON.stringify(users));
       localStorage.setItem('LoggedInuser', JSON.stringify(users));
       alert("Profile has been updated!")
@@ -107,7 +95,54 @@ image.onchange = function(){
   var imgurl=URL.createObjectURL(image.files[0])
  document.querySelector('.imagePreview').src = imgurl
  document.querySelector('#imagePreview').src = imgurl
+ document.querySelector('.post-img').src = imgurl
 }
   
 document.querySelector('#imagePreview').addEventListener("click",function(){
     document.getElementById('profile').click()})
+var createPost = document.querySelector(".create-post")
+var createPostContainer = document.querySelector(".container")
+function createdown() {
+  createPostContainer.style.height = "70vh";
+  createPost.setAttribute("onclick", "createup()");
+}
+function createup() {
+  createPostContainer.style.height = 0;
+  createPost.setAttribute("onclick", "createdown()");
+}
+if(window.addEventListener('click', ()=>{
+  if(loggedin.getAttribute("onclick")=="dropup()")
+  {
+    if(window.addEventListener('click',()=>dropup()))}
+  }))
+
+  function logouthover (){
+    loggedout.src="../Assests/Log out green.png"
+    loggedout.setAttribute("onmouseout","logoutout()")
+  }
+  function logoutout (){
+    loggedout.src="../Assests/Log out.png"
+  }
+  function notificationout(){
+    notification.src = "../Assests/notification.png"
+  }
+  function notificationhover (){
+    notification.src="../Assests/notification green.png"
+    notification.setAttribute("onmouseout","notificationout()")
+  }
+  
+  function setting(){
+    drop.style.height = 0;
+    loggedin.setAttribute("onclick", "dropup()");
+    dropupdate.style.height = "69vh";
+  }
+  
+  function dropdown() {
+    drop.style.height = "31svh";
+    loggedin.setAttribute("onclick", "dropup()");
+  }
+  function dropup() {
+    dropupdate.style.height = 0;
+    drop.style.height = 0;
+    loggedin.setAttribute("onclick", "dropdown()");
+  }
