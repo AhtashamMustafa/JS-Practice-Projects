7caa896cf99b6690eb7e1a284b7b905092cb483e
Ahtasham Mustafa <ahtshammustufa786@gmail.com>
2023-12-04 22:40:01 +0500

    create post

--- Log in & Sign Up Form/Home/home.js
+++ Log in & Sign Up Form/Home/home.js
@@ -49,7 +49,7 @@ if (!loggedInUser) window.location.href = "../Login/Login.html";
 // Display the username on the page
 loggedin.innerHTML = JSON.parse(
   localStorage.getItem("LoggedInuser")
-  )[0].username;
+)[0].username;
 postname.innerHTML = JSON.parse(
   localStorage.getItem("LoggedInuser")
 )[0].username;
@@ -64,44 +64,41 @@ useremail.value = JSON.parse(localStorage.getItem("LoggedInuser"))[0].email;
 usercontact.value = JSON.parse(localStorage.getItem("LoggedInuser"))[0].contactNo;
 usercity.value = JSON.parse(localStorage.getItem("LoggedInuser"))[0].city;
 function update() {
-    var users = JSON.parse(localStorage.getItem("users")) || [];
-    var loggedInUseremail = JSON.parse( 
-      localStorage.getItem("LoggedInuser")
-      ).email;
-document.querySelector('#imagePreview').addEventListener("click",function(){
-  document.getElementById('profile').click()})
-  var index = -1
-  for (let i = 0; i<users.length;i++){
+  var users = JSON.parse(localStorage.getItem("users")) ?? [];
+  var loggedInUseremail = JSON.parse(localStorage.getItem("LoggedInuser")).email;
+  document.querySelector("#imagePreview").addEventListener("click", function () {
+      document.getElementById("profile").click();});
+  var index = -1;
+  for (let i = 0; i < users.length; i++) {
     if (users[i].email == useremail.value) {
       index = i;
-      break
+      break;
     }
   }
-    if (index !== -1) {
-      users[index].username = username.value;
-      users[index].email = useremail.value;
-      users[index].contactNo = usercontact.value;
-      users[index].city = usercity.value;
-      localStorage.setItem('users', JSON.stringify(users));
-      localStorage.setItem('LoggedInuser', JSON.stringify(users));
-      alert("Profile has been updated!")
-      setTimeout(dropup(),2000)
-    }
-
+  if (index !== -1) {
+    users[index].username = username.value;
+    users[index].email = useremail.value;
+    users[index].contactNo = usercontact.value;
+    users[index].city = usercity.value;
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("LoggedInuser", JSON.stringify(users));
+    alert("Profile has been updated!");
+    setTimeout(dropup(), 2000);
+  }
 }
-var image = document.getElementById('profile');
-image.onchange = function(){
+var image = document.getElementById("profile");
+image.onchange = function () {
+  var imgurl = URL.createObjectURL(image.files[0]);
+  document.querySelector(".imagePreview").src = imgurl;
+  document.querySelector("#imagePreview").src = imgurl;
+  document.querySelector(".post-img").src = imgurl;
+};
 
-  var imgurl=URL.createObjectURL(image.files[0])
- document.querySelector('.imagePreview').src = imgurl
- document.querySelector('#imagePreview').src = imgurl
- document.querySelector('.post-img').src = imgurl
-}
-  
-document.querySelector('#imagePreview').addEventListener("click",function(){
-    document.getElementById('profile').click()})
-var createPost = document.querySelector(".create-post")
-var createPostContainer = document.querySelector(".container")
+document.querySelector("#imagePreview").addEventListener("click", function () {
+  document.getElementById("profile").click();
+});
+var createPost = document.querySelector(".create-post");
+var createPostContainer = document.querySelector(".container");
 function createdown() {
   createPostContainer.style.height = "70vh";
   createPost.setAttribute("onclick", "createup()");
@@ -110,39 +107,50 @@ function createup() {
   createPostContainer.style.height = 0;
   createPost.setAttribute("onclick", "createdown()");
 }
-if(window.addEventListener('click', ()=>{
-  if(loggedin.getAttribute("onclick")=="dropup()")
-  {
-    if(window.addEventListener('click',()=>dropup()))}
-  }))
+// if(window.addEventListener('click', ()=>{
+//   if(loggedin.getAttribute("onclick")=="dropup()")
+//   {
+//     if(window.addEventListener('click',()=>dropup()))}
+//   }))
 
-  function logouthover (){
-    loggedout.src="../Assests/Log out green.png"
-    loggedout.setAttribute("onmouseout","logoutout()")
-  }
-  function logoutout (){
-    loggedout.src="../Assests/Log out.png"
-  }
-  function notificationout(){
-    notification.src = "../Assests/notification.png"
-  }
-  function notificationhover (){
-    notification.src="../Assests/notification green.png"
-    notification.setAttribute("onmouseout","notificationout()")
-  }
-  
-  function setting(){
-    drop.style.height = 0;
-    loggedin.setAttribute("onclick", "dropup()");
-    dropupdate.style.height = "69vh";
-  }
-  
-  function dropdown() {
-    drop.style.height = "31svh";
-    loggedin.setAttribute("onclick", "dropup()");
-  }
-  function dropup() {
-    dropupdate.style.height = 0;
-    drop.style.height = 0;
-    loggedin.setAttribute("onclick", "dropdown()");
-  }
+function logouthover() {
+  loggedout.src = "../Assests/Log out green.png";
+  loggedout.setAttribute("onmouseout", "logoutout()");
+}
+function logoutout() {
+  loggedout.src = "../Assests/Log out.png";
+}
+function notificationout() {
+  notification.src = "../Assests/notification.png";
+}
+function notificationhover() {
+  notification.src = "../Assests/notification green.png";
+  notification.setAttribute("onmouseout", "notificationout()");
+}
+
+function setting() {
+  drop.style.height = 0;
+  loggedin.setAttribute("onclick", "dropup()");
+  dropupdate.style.height = "69vh";
+}
+
+function dropdown() {
+  drop.style.height = "31svh";
+  loggedin.setAttribute("onclick", "dropup()");
+}
+function dropup() {
+  dropupdate.style.height = 0;
+  drop.style.height = 0;
+  loggedin.setAttribute("onclick", "dropdown()");
+}
+const container = document.querySelector(".container");
+let privacy = container.querySelector(".post .privacy");
+let arrowBack = container.querySelector(".audience .arrow-back");
+
+privacy.addEventListener("click", () => {
+  container.classList.add("active");
+});
+
+arrowBack.addEventListener("click", () => {
+  container.classList.remove("active");
+});
